3b0082795493bac64ad2e1d58074fc4e2943afcb
Francesca Paoletti <frapao@amazon.com>
2019-11-13 15:43:21 -0800

    Opendistro-1.3 compatible with ES and Kibana 7.3

--- README.md
+++ README.md
@@ -1,6 +1,65 @@
-## Open Distro for Elasticsearch SQL Kibana Plugin
+# Open Distro for Elasticsearch SQL Kibana
+
+The Open Distro for Elasticsearch SQL Kibana plugin lets you query your Elasticsearch data using SQL syntax from a dedicated Kibana UI. You can then download data in JSON, JDBC, or CSV format.
+
+
+## Documentation
+
+Please see our [documentation](https://opendistro.github.io/for-elasticsearch-docs/).
+
+
+## Setup
+
+1. Download Elasticsearch for the version that matches the [Kibana version specified in package.json](./package.json#L8).
+1. Download and install the appropriate [Open Distro for Elasticsearch Sql plugin](https://github.com/opendistro-for-elasticsearch/sql).
+1. Download the Kibana source code for the [version specified in package.json](./package.json#L8) you want to set up.
+
+   See the [Kibana contributing guide](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#setting-up-your-development-environment) for more instructions on setting up your development environment.
+   
+1. Change your node version to the version specified in `.node-version` inside the Kibana root directory.
+1. Create a `kibana-extra` directory as a sibling directory to the Kibana source code directory.
+1. Check out this package from version control into the `kibana-extra` directory.
+1. Run `yarn kbn bootstrap` inside `kibana-extra/opendistro-elasticsearch-sql-kibana`.
+
+Ultimately, your directory structure should look like this:
+
+```md
+.
+├── kibana
+├── kibana-extra
+│   └── opendistro-elasticsearch-sql-kibana
+```
+
+
+## Build
+
+To build the plugin's distributable zip simply run `yarn build`.
+
+Example output: `./build/opendistro-kibana-sql-1.0.0.0.zip`
+
+
+## Run
+
+- `yarn start`
+
+  Starts Kibana and includes this plugin. Kibana will be available on `localhost:5601`.
+
+- `NODE_PATH=../../kibana/node_modules yarn test:jest`
+
+  Runs the plugin tests.
+
+
+## Contributing to Open Distro for Elasticsearch SQL Kibana
+
+- Refer to [CONTRIBUTING.md](./CONTRIBUTING.md).
+- Since this is a Kibana plugin, it can be useful to review the [Kibana contributing guide](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md) alongside the documentation around [Kibana plugins](https://www.elastic.co/guide/en/kibana/master/kibana-plugins.html) and [plugin development](https://www.elastic.co/guide/en/kibana/master/plugin-development.html).
+
 
 
 ## License
 
-This library is licensed under the Apache 2.0 License. 
+This code is licensed under the Apache 2.0 License. 
+
+## Copyright
+
+Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
---
+++ public/components/QueryEditor/QueryEditor.tsx
@@ -0,0 +1,146 @@
+/*
+ *   Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+import React from "react";
+import _ from "lodash";
+// @ts-ignore
+import { EuiPanel, EuiTitle, EuiButton, EuiText, EuiFlexGroup, EuiFlexItem, EuiCodeEditor, EuiHeaderLink} from "@elastic/eui/lib";
+import "brace/mode/mysql";
+import "brace/mode/json";
+import "../../ace-themes/sql_console";
+import { ResponseDetail, TranslateResult } from "../Main/main";
+
+interface QueryEditorProps {
+  onRun: (queriesString: string) => void;
+  onTranslate: (queriesString: string) => void;
+  onClear: () => void;
+  queryTranslations: ResponseDetail<TranslateResult>[];
+  sqlQueriesString: string;
+}
+
+interface QueryEditorState {
+  sqlQueriesString: string;
+}
+
+class QueryEditor extends React.Component<QueryEditorProps, QueryEditorState> {
+  constructor(props: QueryEditorProps) {
+    super(props);
+    this.state = {
+      sqlQueriesString: this.props.sqlQueriesString ? this.props.sqlQueriesString : ""
+    };
+
+    this.updateSQLQueries = _.debounce(this.updateSQLQueries, 250).bind(this);
+  }
+
+  updateSQLQueries(newQueriesString: string): void {
+    this.setState({ sqlQueriesString: newQueriesString });
+  }
+
+  render() {
+    return (
+      <EuiPanel className="sql-console-query-editor container-panel"
+        paddingSize="none"
+      >
+        <EuiTitle className="container-panel-header" size="l">
+          <h1>SQL console</h1>
+        </EuiTitle>
+
+        <EuiFlexGroup gutterSize="none">
+          <EuiFlexItem grow={1} className="sql-query-panel" paddingSize="none">
+            <EuiText className="sql-query-panel-header">SQL</EuiText>
+            <EuiCodeEditor
+              mode="mysql"
+              theme="sql_console"
+              width="100%"
+              height="18.5rem"
+              value={this.state.sqlQueriesString}
+              onChange={this.updateSQLQueries}
+              showPrintMargin={false}
+              setOptions={{
+                fontSize: "12px",
+                enableBasicAutocompletion: true,
+                enableSnippets: true,
+                enableLiveAutocompletion: true
+              }}
+              aria-label="Code Editor"
+            />
+          </EuiFlexItem>
+          <EuiFlexItem
+            grow={1}
+            className="translated-query-panel"
+            paddingSize="none"
+          >
+            <EuiText className="translated-query-panel-header">
+              Elasticsearch query string
+            </EuiText>
+            <EuiCodeEditor
+              mode="json"
+              theme="sql_console"
+              width="100%"
+              height="18.5rem"
+              value={this.props.queryTranslations
+                .map((queryTranslation: any) =>
+                  JSON.stringify(queryTranslation.data, null, 2)
+                )
+                .join("\n")}
+              showPrintMargin={false}
+              readOnly={true}
+              setOptions={{
+                fontSize: "12px",
+                readOnly: true,
+                highlightActiveLine: false,
+                highlightGutterLine: false
+              }}
+              aria-label="Code Editor"
+            />
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        <div>
+          <EuiFlexGroup className="action-container" gutterSize="m">
+            <EuiFlexItem
+              grow={false}
+              onClick={() => this.props.onRun(this.state.sqlQueriesString)}
+            >
+              <EuiButton fill={true} className="sql-editor-button" >
+                Run
+              </EuiButton>
+            </EuiFlexItem>
+            <EuiFlexItem
+              grow={false}
+              onClick={() =>
+                this.props.onTranslate(this.state.sqlQueriesString)
+              }
+            >
+              <EuiButton className="sql-editor-button">Translate</EuiButton>
+            </EuiFlexItem>
+            <EuiFlexItem
+              grow={false}
+              onClick={() => {
+                this.updateSQLQueries("");
+                this.props.onClear();
+              }}
+            >
+              <EuiHeaderLink className="sql-editor-link" href="#" isActive>
+                Clear
+              </EuiHeaderLink>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </div>
+      </EuiPanel>
+    );
+  }
+}
+
+export default QueryEditor;
